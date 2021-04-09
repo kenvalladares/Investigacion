ae874346fe074fa18f73c3075f94d297459b2f59
kenvalladares <71302399+kenvalladares@users.noreply.github.com>
2021-04-09 17:21:31 -0600

    Add files via upload

---
+++ PII_2021_I_P3_LOGS/src/hn/edu/ujcv/pii/p3/Main.java
@@ -0,0 +1,16 @@
+package hn.edu.ujcv.pii.p3;
+
+import org.apache.log4j.Logger;
+
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
+    public static void main(String[] args) throws IOException {
+
+        log.debug("hola");
+    }
+}
+
